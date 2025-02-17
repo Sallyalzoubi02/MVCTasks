1c8e5d2009c2431460b6a6d21a4f915391ab2ffa
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-02-17 22:13:56 +0300

    Add project files.

---
+++ task/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace task.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult UsersList()
+        {
+            return View();
+        }
+    }
+}
---
+++ task/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace task.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult RoomsList()
+        {
+            return View();
+        }
+    }
+}
