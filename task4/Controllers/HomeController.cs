800c5539aea8b4715c520dea2a05895c60becdd1
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-02-19 23:37:39 +0300

    commit

---
+++ task4/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using task4.Models;
+
+namespace task4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string Username = HttpContext.Session.GetString("UserName");
+            ViewData["UserName"] = Username;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ task4/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace task4.Controllers
+{
+    public class UserController : Controller
+    {
+        
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult HandelRegister()
+        {
+            string name = Request.Form["name"];
+            string email = Request.Form["email"];
+            string pass = Request.Form["password"];
+            string repeat = Request.Form["repeatPassword"];
+
+            if (pass == repeat)
+            {
+                HttpContext.Session.SetString("UserName", name);
+                HttpContext.Session.SetString("UserEmail", email);
+                HttpContext.Session.SetString("UserPassword", pass);
+                return RedirectToAction("Login");
+                
+            }
+            else
+            {
+                TempData["alert"] = "password not match ";
+                return RedirectToAction("Register");
+            }
+        }
+
+        public IActionResult HandelLogin()
+        {
+            string email = Request.Form["email"];
+            string pass = Request.Form["password"];
+
+            string storedEmail = HttpContext.Session.GetString("UserEmail");
+            string storedPassword = HttpContext.Session.GetString("UserPassword");
+
+            if (pass == storedPassword && email == storedEmail)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                TempData["alert"] = "password not match ";
+                return RedirectToAction("Register");
+            }
+        }
+        public IActionResult Register()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            
+            return View();
+        }
+    }
+}
