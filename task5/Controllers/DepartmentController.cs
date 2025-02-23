d179eeb8c85945e9fa07698622bea61cd80cc137
Sallyalzoubi02 <alzoubisally0@gmail.com>
2025-02-23 18:33:55 +0300

    commit

---
+++ task5/Controllers/DepartmentController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using task5.Models;
+
+namespace task5.Controllers
+{
+    public class DepartmentController : Controller
+    {
+
+        private readonly MyDbContext _context;
+
+        public DepartmentController(MyDbContext context)
+        {
+            _context = context;
+        }
+        // GET: UserController
+        public ActionResult Index()
+        {
+            return View(_context.Departments.ToList());
+        }
+
+     
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Department department)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Departments.Add(department);
+                    _context.SaveChangesAsync();                    
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return View(department);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ task5/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task5.Models;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Location { get; set; }
+
+    public int? ManagerId { get; set; }
+
+    public DateOnly? EstablishedDate { get; set; }
+}
